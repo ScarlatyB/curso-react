dc61082b978f2f7dd08953e6ce9b7e9c8d61ee06
scarfada <83604371+scarfada@users.noreply.github.com>
2022-10-04 13:46:07 -0300

    criando componentes e aprendendo a usar props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
+import Frase from './components/Frase';
+import UsandoProps from './components/UsandoProps';
 
 export default function App() {
   return (
     <div>
+      <Frase />
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+      <UsandoProps nome="Scar" idade="26" estadocivil="namorando" />
     </div>
   );
 }
---
+++ src/components/Frase.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Frase() {
+  return (
+    <div>
+      <p>Isso é um parágrafo!</p>
+    </div>
+  );
+}
